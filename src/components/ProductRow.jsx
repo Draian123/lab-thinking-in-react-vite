46340e30c44e0893eaaac67aacd1483237ea7375
Draian123 <beiteldennis3@yahoo.de>
2023-02-22 20:29:30 +0100

    done

--- src/components/ProductRow.jsx
+++ src/components/ProductRow.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 export default function ProductRow({product}) {
   return (
     <tr>
-        {product.inStock? <td>{product.name}</td>: <td style={{"color": "red"}}>{product.name}</td>}
+        {product.inStock ? <td>{product.name}</td> : <td style={{"color": "red"}}>{product.name}</td>}
         <td>{product.price}</td>
     </tr>
   )
 }
+
